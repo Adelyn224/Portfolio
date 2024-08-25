c614ff53032a89a9724d1abf11de3dbe892eec8f
Adelyn224 <153022581+Adelyn224@users.noreply.github.com>
2024-08-25 15:16:28 +0100

    modified index.html, style.css, and script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("testing");
+const toggleThemeButton = document.querySelector(".dark-mode-button");
+
+function handleThemeFunction() {
+    const isDarkMode = document.body.classList.toggle("dark-mode");
+    const newSrc = isDarkMode?
+    "./assets/images/moon.png":
+    "./assets/images/sun.png";
+    toggleThemeButton.setAttribute("src", newSrc);
+}
+
+toggleThemeButton.addEventListener("click", handleThemeFunction);
